8a615ab1f8c45eea0d2a2755fa4770c6451bb859
imminentgloom <67942961+imminentgloom@users.noreply.github.com>
2024-08-25 14:53:48 +0200

    Update mod.lua
    set defaults

--- lib/mod.lua
+++ lib/mod.lua
@@ -9,13 +9,13 @@ local FxDelay7 = fx:new{
 }
 
 function FxDelay7:add_params()
-	params:add_separator("fx_delay7", "fx delayyyyyyyy")
+    params:add_separator("fx_delay7", "fx delay7")
 	FxDelay7:add_slot("fx_delay7_slot", "slot")
-	FxDelay7:add_control("fx_delay7_time", "time", "time", controlspec.new(0.0001, 2, "exp", 0, 0.3, "s"))
-	FxDelay7:add_control("fx_delay7_feedback", "feedback", "feedback", controlspec.new(0, 1, "lin", 0.01, 0, ""))
+	FxDelay7:add_control("fx_delay7_time", "time", "time", controlspec.new(0.0001, 2, "exp", 0, 0.55, "s"))
+	FxDelay7:add_control("fx_delay7_feedback", "feedback", "feedback", controlspec.new(0, 1, "lin", 0.01, 0.8, ""))
 	FxDelay7:add_control("fx_delay7_sep", "sep", "sep", controlspec.new(0, 1, "lin", 0.01, 0, ""))
-	FxDelay7:add_control("fx_delay7_mix", "mix", "mix", controlspec.new(0, 1, "lin", 0.01, 0, ""))
-	FxDelay7:add_control("fx_delay7_send", "send", "send", controlspec.new(0, 1, "lin", 0.01, 0, ""))
+	FxDelay7:add_control("fx_delay7_mix", "mix", "mix", controlspec.new(0, 1, "lin", 0.01, 0.4, ""))
+	FxDelay7:add_control("fx_delay7_send", "send", "send", controlspec.new(0, 1, "lin", 0.01, 0.8, ""))
 	FxDelay7:add_control("fx_delay7_hp", "hp", "hp", controlspec.new(20, 10000, "exp", 0, 20, "Hz"))
 	FxDelay7:add_control("fx_delay7_lp", "lp", "lp", controlspec.new(20, 10000, "exp", 0, 5000, "Hz"))
 end
